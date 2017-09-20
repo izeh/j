e2e14e1a57e302c563cd06b378fbafcac007b1d1
izeh <31554814+izeh@users.noreply.github.com>
2017-09-20 16:03:29 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -1,7 +1,6 @@
  p.es = function(n, es, n.sim){
   
 t.sim = Vectorize(function(n, es){
-    
     d = numeric(n.sim)
     p = numeric(n.sim)
     
@@ -11,8 +10,7 @@ for(i in 1:n.sim){
     y = rnorm(n, 0, 1)
     a = t.test(x, y, var.equal = TRUE)
  d[i] = a[[1]]/N
- p[i] = a[[3]]
-    }
+ p[i] = a[[3]] }
 h = hist(p, breaks = 15, xlab = "p-value", main = paste0("sig. p-values = ", mean(p <= .05)*1e2, "%"))
 rect(0, 0, h$breaks[2], h$counts[1], col = rgb(0, 0, 1, .4), border = 2)
 mtext(paste0("Group Sample Size = ", n), 3, font = 2, cex = .7, xpd = NA)
@@ -21,9 +19,7 @@ hist(d, xlab = "Cohen's d", main = NA) ; abline(v = es, col = 2, lty = 2) ; axis
   }, c("n", "es"))
   
   input = if(length(n) > 1) n else if(length(es) > 1) es else n
-  
   par(mfcol = c(2, length(input)), font.lab = 2)
-  invisible(t.sim(n, es))
-}
+  invisible(t.sim(n, es)) }
 # Example of use:
 p.es(n = c(20, 50, 100), es = .1, n.sim = 1e3)
